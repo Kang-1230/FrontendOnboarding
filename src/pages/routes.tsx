6370643d92da1995753f46dfdac029c4cff140d0
KangSujin <tnwls7138@gmail.com>
2025-02-17 13:37:08 +0900

    fix : navbar middleware 적용 하려다 불편해서 outlet으로 바꿈

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -3,16 +3,16 @@ import Home from "./Home";
 import Signup from "./SignUp";
 import Login from "./Login";
 import Mypage from "./Mypage";
-// import MainLayout from "../Layout/MainLayout";
+import MainLayout from "../Layout/MainLayout";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route element={<MainLayout />}> */}
-        <Route path="/" element={<Home />} />
-        <Route path="/mypage/:id" element={<Mypage />} />
-        {/* </Route> */}
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/mypage/:id" element={<Mypage />} />
+        </Route>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
